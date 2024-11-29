b5edd62c39ff9fda9ff397bec75787836065cea8
savv <mrpetuch@yandex.ru>
2024-11-29 21:54:20 +0300

    app + test

---
+++ string_editor.py
@@ -0,0 +1,18 @@
+def add(*string):
+    # возвращает слитую строку
+    res = ''
+    for i in string:
+        res += str(i)
+    return res
+
+def capital(string):
+    # возвращает строку с заглавной буквы
+    if type(string) != str:
+        raise ValueError('Введена не строка')
+    return string.capitalize()
+
+def up(string):
+    # возвращает строку в верхнем регистре
+    if type(string) != str:
+        raise ValueError('Введена не строка')
+    return string.upper()
