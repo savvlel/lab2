247ec18a9841d962444f3fdd5856236f375f55be
savvlel <162036962+savvlel@users.noreply.github.com>
2024-11-29 22:01:40 +0300

    Update string_test.py

--- string_test.py
+++ string_test.py
@@ -1,20 +1,24 @@
 import pytest
 from string_editor import add, capital, up
 
+
 def test_add():
     assert add('a', 'b') == 'ab'
     assert add(2, 3) =='23'
     assert add('a', 1, 'b') == 'a1b'
 
+
 def test_capital():
     assert capital('aaa') == 'Aaa'
     assert capital('AAA') == 'AAA'
     assert capital('123') == '123'
-    
+
+
 def test_up():
     assert capital('aaa') == 'AAA'
     assert capital('AAA') == 'AAA'
     assert capital('123') == '123'
+
 
 def test_not_string():
     with pytest.raises(ValueError):
