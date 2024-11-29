d614acfc45698df381528db840722ec7d0b75b42
savvlel <162036962+savvlel@users.noreply.github.com>
2024-11-29 22:01:18 +0300

    Update string_editor.py

--- string_editor.py
+++ string_editor.py
@@ -5,11 +5,13 @@ def add(*string):
         res += str(i)
     return res
 
+
 def capital(string):
     # возвращает строку с заглавной буквы
     if type(string) != str:
         raise ValueError('Введена не строка')
     return string.capitalize()
+
 
 def up(string):
     # возвращает строку в верхнем регистре
